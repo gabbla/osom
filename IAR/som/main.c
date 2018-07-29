95a689ec873771a2258f30297692102a00845d93
Gabbla <zampierigabriele.ist@mail.com>
2018-07-29 11:04:41 +0200

    First IAR workspace draft THAT COMPILES and LINK A no-limit license is needed

---
+++ IAR/som/main.c
@@ -0,0 +1,13 @@
+#include <hal_types.h>
+#include "ioCC2541.h"
+
+#include "OSAL.h"
+#include "OSAL_Tasks.h"
+#include "OSAL_PwrMgr.h"
+#include "osal_snv.h"
+//#include "OnBoard.h"
+
+int main( void )
+{
+  return 0;
+}
